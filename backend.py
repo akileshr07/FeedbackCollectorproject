377cccd3be81debad8068a8888b7776b0955a03a
AKILESH R <166041192+akileshr07@users.noreply.github.com>
2025-07-03 12:32:16 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -23,7 +23,7 @@ app.add_middleware(
 # --- Database Setup ---
 # DATABASE_URL = "sqlite:///./feedback.db"
 # engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-# Base = declarative_base()
+Base = declarative_base()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'feedback.db')}"
